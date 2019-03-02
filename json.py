9d6af1b9d43640b2bc39429327953a4d19bf65ac
Muppavaram Sai Raghavendra <raghava.mittu@gmail.com>
2019-03-02 08:31:09 +0530

    Create json.py
    This Code explains how to convert json to python and python to json

---
+++ json.py
@@ -0,0 +1,13 @@
+import json
+#Coverting JSON to Python
+x = '{"id":456,"company":"Google","project":"Solar"}'
+y = json.loads(x)
+#The Result is a Python Dictonary
+print(y)
+
+---------------------------------------------------
+#Converting Python to JSON
+x = {"id":456,"company":"Google","project":"Solar"}
+y = json.dumps(x)
+#Printin the resultant json
+print(y)
