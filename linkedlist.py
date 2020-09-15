2d69900716cb32e61fa12314bd57c216b6d5b50b
Muppavaram Sai Raghavendra <raghava.mittu@gmail.com>
2020-09-15 21:47:59 +0530

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -16,6 +16,13 @@ class SLinkedList:
         while x is not None:
             print(x.dataval)
             x = x.nextval
+    
+    def insertnode(self):
+        
+        #Here we need to create a new node that we want to insert
+        e4 = Node('Fri')
+        self.headval.nextval = e4
+        e4.nextval = e2
 
 list1 = SLinkedList()
 list1.headval = Node('Mon')
@@ -23,4 +30,5 @@ e2 = Node('Tue')
 e3 = Node('Wed')
 list1.headval.nextval = e2
 e2.nextval = e3
+list1.insertnode()
 list1.printval()
